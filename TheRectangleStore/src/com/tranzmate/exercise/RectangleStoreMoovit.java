bdaf80c21be330e43658ec524c08d5a0a882b4cf
Lior Rahamim <kenlelior@gmail.com>
2020-04-28 15:44:39 +0300

    Copied necessary files to project

---
+++ TheRectangleStore/src/com/tranzmate/exercise/RectangleStoreMoovit.java
@@ -0,0 +1,23 @@
+package com.tranzmate.exercise;
+
+import java.util.Collection;
+
+public class RectangleStoreMoovit implements IRectanglesStore {
+
+	@Override
+	public void initialize(IRectangle bounds, Collection<IRectangle> rectangles) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * return the topmost rectangle per specified x, y location 
+	 * (or null in case no rectangle exists in the specified location) 
+	 */
+	@Override
+	public IRectangle findRectangleAt(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
